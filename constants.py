71ffe9bc1dd5dffe0973c69cd5e0e1225529ea9f
Leo Predator <leochang658@gmail.com>
2023-12-15 18:20:30 +0800

    FEAT: check json data amount

--- constants.py
+++ constants.py
@@ -1,7 +1,5 @@
 json_output_folder = "loss_data"
 graph_output_folder = "analysis"
 
-style_list = ["Abstract_Expressionism", "Baroque",
-              "Cubism", "Fauvism", "Pointillism", "Ukiyo_e"]
-
+style_list = ["Cubism", "Fauvism", "Pointillism", "Ukiyo_e"]
 loss_list = ["clip", "gram_matrix"]
---
+++ json_check.py
@@ -0,0 +1,26 @@
+import json
+from constants import loss_list, json_output_folder
+from path import path_join
+
+method_list = ["AdaIN"]
+style_list = ["Fauvism", "Pointillism"]
+content_amount = 20
+style_amount = 100
+required_amount = content_amount * style_amount
+
+for method in method_list:
+    for style in style_list:
+        for loss in loss_list:
+            file_name = f"{style}-{loss}.json"
+            print(f"{file_name}")
+            json_file = path_join(json_output_folder, method, file_name)
+            with open(json_file, 'r') as json_file:
+                data = json.load(json_file)
+            clip_loss_amount = 0
+            for clip_loss in data:
+                clip_loss_amount = clip_loss_amount + 1
+            if clip_loss_amount != required_amount:
+                raise Exception(
+                    f"Data amount in {style}-{loss}.json is below {required_amount}")
+
+print("JSON verified")
